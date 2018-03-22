4b1b4549184c37db5099407ba84a4bc4ab035cb8
Leah Gerald <Leanator13@gmail.com>
2018-03-22 17:20:59 -0400

    seed file: includes matches

--- script/seed.js
+++ script/seed.js
@@ -10,7 +10,7 @@
  * Now that you've got the main idea, check it out in practice below!
  */
 const db = require('../server/db')
-const {User} = require('../server/db/models')
+const {User, Matches, Messages} = require('../server/db/models')
 
 async function seed () {
   await db.sync({force: true})
@@ -19,12 +19,38 @@ async function seed () {
   // executed until that promise resolves!
 
   const users = await Promise.all([
-    User.create({email: 'cody@email.com', password: '123'}),
-    User.create({email: 'murphy@email.com', password: '123'})
+    User.create({ email: 'cody@email.com', userName: 'cody', age: 21, borough: 'Brooklyn', image: "https://fthmb.tqn.com/nkdfSTqlBSF-kWa-0RE9PcmFz7k=/768x0/filters:no_upscale()/feet-face-599c412c22fa3a0011d92add.jpg", password: '123'}),
+    User.create({ email: 'murphy@email.com', userName: 'murph', age: 18, borough: 'Manhattan', image: "https://fthmb.tqn.com/-XOWjEiELkzreHYCMjoyrTEEwjU=/768x0/filters:no_upscale()/loan-599c450a03f40200117e717a.jpg", password: '123'}),
+    User.create({ email: 'sarah@email.com', userName: 'tadbole', age: 11, borough: 'Bronx', image: "https://previews.123rf.com/images/nomadsoul1/nomadsoul11512/nomadsoul1151200103/49820654-professor-in-glasses-thinking-about-math-formulas.jpg", password: '123' }),
+    User.create({ email: 'Leah@email.com', userName: 'Lee', age: 5, borough: 'Manhattan', image: "https://timedotcom.files.wordpress.com/2017/02/170206_badstock_17.jpg", password: '123' }),
+    User.create({ email: 'Lemona@email.com', userName: 'blockchain_Babe', borough: 'Long Island', image: "http://agrandeescola.com.br/wp-content/uploads/2017/08/Comunica%C3%A7%C3%A3o_Assertiva.png", age: 30, password: '123' }),
+    User.create({ email: 'Bob@email.com', userName: 'Bob', age: 23, borough: 'Brooklyn', image: "https://i.ytimg.com/vi/JfrtU4Wdz9Y/maxresdefault.jpg", password: '123' }),
+    User.create({ email: 'alex@email.com', userName: 'alex', age: 45, borough: 'Brooklyn', image: "https://img.buzzfeed.com/buzzfeed-static/static/2014-05/enhanced/webdr07/14/7/original-23502-1400067193-9.jpg?downsize=715:*&output-format=auto&output-quality=auto", password: '123' }),
+    User.create({ email: 'noah@email.com', userName: 'the_baker', age: 26, borough: 'Bronx', image: "https://i.pinimg.com/736x/33/6e/2e/336e2e91f8bc1fe91c0de80ae6019fb6--photos-tumblr-tumblr-posts.jpg", password: '123' }),
+    User.create({ email: 'hannah@email.com', userName: 'turtle_neck', age: 26, borough: 'Brooklyn', image: "https://hookagency.com/wp-content/uploads/2017/07/worst-stock-photos-for-memes.jpg", password: '123' }),
+    User.create({ email: 'Julia@email.com', userName: 'baby_Jaspers', age: 23, borough: 'Brooklyn', image: "https://s-i.huffpost.com/gadgets/slideshows/283408/slide_283408_2160763_free.jpg", password: '123' })
   ])
+  const matches = await Promise.all([
+    Matches.create({round: 1, selector: 5, selected: 3}),
+    Matches.create({ round: 1, selector: 2, selected: 4}),
+    Matches.create({ round: 2, selector: 9, selected: 7}),
+    Matches.create({ round: 3, selector: 7, selected: 2}),
+    Matches.create({ round: 3, selector: 10, selected: 5})
+
+  ])
+  // const messages = await Promise.all([
+  //   Messages.create({}),
+  //   Messages.create({}),
+  //   Messages.create({}),
+  //   Messages.create({}),
+  //   Messages.create({}),
+  //   Messages.create({}),
+
+  // ])
   // Wowzers! We can even `await` on the right-hand side of the assignment operator
   // and store the result that the promise resolves to in a variable! This is nice!
   console.log(`seeded ${users.length} users`)
+  console.log(`seeded ${matches.length} matches`)
   console.log(`seeded successfully`)
 }
 
