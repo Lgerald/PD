40a816525981275ee886dcdb11eb1e319f1da75d
Leah Gerald <Leanator13@gmail.com>
2018-03-28 12:41:42 -0400

    omg yaz i can haz seed file

--- client/components/User/profile.jsx
+++ client/components/User/profile.jsx
@@ -1,9 +1,34 @@
 import React from 'react'
+import db from '../../Fire'
+import firebase from 'firebase'
+const docRef = firebase
+  .firestore()
+  .collection('users')
+  .doc('beep')
 
-const Profile = () => {
+class Profile extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {}
+  }
+  componentDidMount() {
+    this.unsubscribe = firebase.auth()
+  }
+  handleClick = () => {
+    docRef
+      .set({
+        beep: 'merp'
+      })
+      .then(() => console.log(`it did a thing`))
+      .catch(err => console.error(err))
+  }
+  render() {
     return (
-        <div>ITSA ME: PROFILE PAGE</div>
+      <div>
+        <button onClick={this.handleClick()}>CLICK ME</button>
+      </div>
     )
+  }
 }
 
 export default Profile
